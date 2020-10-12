e0dec076c0a72819a2b6b1498371c988c5f16391
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2020-10-12 00:47:55 -0500

    Updates

---
+++ precache-manifest.aa53d60bf17eecd10de9cd3c5b732983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5807be662942039d24c0e6298b6c9040",
+    "url": "/auth-system-react/index.html"
+  },
+  {
+    "revision": "90a65183b331c48dda2b",
+    "url": "/auth-system-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "602dced32d81cf4e0235",
+    "url": "/auth-system-react/static/js/2.1e466e59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/auth-system-react/static/js/2.1e466e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a65183b331c48dda2b",
+    "url": "/auth-system-react/static/js/main.df6bd7b6.chunk.js"
+  },
+  {
+    "revision": "afbd960a5effc45c4154",
+    "url": "/auth-system-react/static/js/runtime-main.f43401e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth-system-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
